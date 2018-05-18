36046778744008418268b06c5687568d00f76c45
Michael <coderliq@gmail.com>
2018-05-19 00:30:02 +0500

    Автоматическое присвоение авторства при создании нового желания

--- mydesire/Controllers/WishesController.cs
+++ mydesire/Controllers/WishesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,10 +16,12 @@ namespace mydesire.Controllers
     public class WishesController : Controller
     {
         private readonly ApplicationDbContext _context;
-
-        public WishesController(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+            
+        public WishesController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [AllowAnonymous]
@@ -67,6 +70,7 @@ namespace mydesire.Controllers
         {
             if (ModelState.IsValid)
             {
+                wish.IssuerId = _userManager.GetUserId(User);
                 _context.Add(wish);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
